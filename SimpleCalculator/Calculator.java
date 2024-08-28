c39d2a815ba757a96afdf8b8d4066099ef79f719
Manure Arjun <113349576+ArjunManure@users.noreply.github.com>
2024-08-28 21:19:50 +0530

    Create Calculator.java

---
+++ SimpleCalculator/Calculator.java
@@ -0,0 +1,51 @@
+import java.lang.*;
+import java.util.Scanner;
+
+
+public class Calculator {
+
+    public static void main(String[] args)
+    {
+
+        double num1, num2;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the numbers:");
+
+        num1 = sc.nextDouble();
+        num2 = sc.nextDouble();
+
+        System.out.println("Enter the operator (+,-,*,/):");
+
+        char op = sc.next().charAt(0);
+        double o = 0;
+
+        switch (op) {
+            case '+':
+                o = num1 + num2;
+                break;
+
+            case '-':
+                o = num1 - num2;
+                break;
+
+            case '*':
+                o = num1 * num2;
+                break;
+
+            case '/':
+                o = num1 / num2;
+                break;
+
+            default:
+                System.out.println("You enter wrong input");
+        }
+
+        System.out.println("The final result:");
+        System.out.println();
+
+        System.out.println(num1 + " " + op + " " + num2
+                + " = " + o);
+    }
+}
